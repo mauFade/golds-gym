a22db97f5f3ee150196082635148e429d4184f1b
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-06-18 22:43:19 -0300

    fixes

--- src/components/BodyPart.tsx
+++ src/components/BodyPart.tsx
@@ -11,27 +11,37 @@ interface IPropsBodyPart {
 const BodyPart = (Props: IPropsBodyPart) => {
   return (
     <Stack
-      type="button"
       alignItems="center"
       justifyContent="center"
       className="bodyPart-card"
-      sx={{
-        borderTop: Props.bodyPart === Props.item ? "4px solid #FF2625" : "",
-        backgroundColor: Props.bodyPart === Props.item ? "#DEDEDE" : "#FFF",
-        borderBottomLeftRadius: "20px",
-        borderBottomRightRadius: "20px",
-        width: "270px",
-        height: "280px",
-        cursor: "pointer",
-        gap: "47px",
-      }}
+      sx={
+        Props.bodyPart === Props.item
+          ? {
+              borderTop: "4px solid #FF2625",
+              background: "#fff",
+              borderBottomLeftRadius: "20px",
+              width: "270px",
+              height: "282px",
+              cursor: "pointer",
+              gap: "47px",
+            }
+          : {
+              background: "#fff",
+              borderBottomLeftRadius: "20px",
+              width: "270px",
+              height: "282px",
+              cursor: "pointer",
+              gap: "47px",
+            }
+      }
       onClick={() => {
         Props.setBodyPart(Props.item);
         window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
       }}
     >
-      <img src={Icon} alt="icon" style={{ width: "40px", height: "40px" }} />
-      <Typography fontSize="24px" fontWeight="bold" color="3A1212" textTransform="capitalize">
+      <img src={Icon} alt="dumbbell" style={{ width: "40px", height: "40px" }} />
+      <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize">
+        {" "}
         {Props.item}
       </Typography>
     </Stack>
